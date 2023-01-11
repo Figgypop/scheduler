c4dcfbf0f699fe09b479a391410ec37cdedfc5b7
David Figueroa <figueroadavid93@gmail.com>
2023-01-11 17:06:16 -0500

    implemented components and their stories for appointment: Empty, Show, Confirm, Status, Error

---
+++ src/components/Appointment/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+/*
+time:String - the time of the appointment (e.g "12pm")
+*/
+
+export default function Header(props) {
+  return (
+    <header className="appointment__time">
+      <h4 className="text--semi-bold">{props.time}</h4>
+      <hr className="appointment__separator" />
+    </header>
+  )
+}
