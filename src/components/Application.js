7ef0d5ec966445bc1d1c3e09adcd3353187c9241
David Figueroa <figueroadavid93@gmail.com>
2023-01-12 13:35:50 -0500

    Appointment component rendered on broswer with mock data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,8 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
+import Appointment from "./Appointment";
 
 const days = [
   {
@@ -21,8 +22,47 @@ const days = [
   },
 ];
 
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
+
 export default function Application(props) {
-const [day, setDay] = useState("Monday")
+  const [day, setDay] = useState("Monday")
 
   return (
     <main className="layout">
@@ -32,12 +72,12 @@ const [day, setDay] = useState("Monday")
           src="images/logo.png"
           alt="Interview Scheduler"
         />
-      <nav>
-        <DayList
-        days={days}
-        value={day}
-        onChange={setDay} />
-      </nav>
+        <nav>
+          <DayList
+            days={days}
+            value={day}
+            onChange={setDay} />
+        </nav>
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu"></nav>
         <img
@@ -47,7 +87,15 @@ const [day, setDay] = useState("Monday")
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {Object.values(appointments).map(appointment => {
+          return (
+            <Appointment
+              key={appointment.id}
+              {...appointment}
+            />
+          )
+        })}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
